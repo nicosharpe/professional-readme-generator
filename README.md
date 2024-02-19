061614b8e0c086bd249cc6a5d25b59148effc59b
nicosharpe <nicolesharpe11@gmail.com>
2024-02-19 22:05:21 +0000

    added packages, wrote js, readme works

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# professional-readme-generator
-A generator for a professional looking README file.  Simply answer the questions and the document will build itself. 
+![GitHub License](https://img.shields.io/github/license/nicosharpe/blah blah)
+
+  # blah blah
+
+  blah
+
+  # Table of Contents
+  [License](https://github.com/nicosharpe/readme#license)
+
+  [Installation](https://github.com/nicosharpe/readme#installation)
+
+  [Usage](https://github.com/nicosharpe/readme#usage)
+
+  [Contributions](https://github.com/nicosharpe/readme#contributions)
+
+  [Tests](https://github.com/nicosharpe/readme#tests)
+
+  [Questions](https://github.com/nicosharpe/readme#questions)
+
+  
+
+  # License
+
+  MIT License
+
+  # Installation
+
+  To install the dependencies needed in your coding software in order to use this generator, simply type npm i into your terminal and press enter.
+
+  # Tests
+
+  To test if the packages are working, type npm test into your terminal and press enter.
+
+  # Usage
+
+  nothing
+
+  # Contributions
+
+  nothing
+
+
+  # Questions
+
+  If you have any questions about this project, feel free to reach me via github:
+  
+  nicosharpe (https://github.com/nicosharpe/)
+
+  Or you can send me a message via email:
+  nicolesharpe11@gmail.com
+  
+  
---
+++ index.js
@@ -0,0 +1,121 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+const { makeBadge, ValidationError } = require('badge-maker')
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+
+
+const promptUser = () =>
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'username',
+      message: 'What is your GitHub username?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email address?',
+    },
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the reposition titled?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Please write a short description of your project.',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'What kind of license should your project have?',
+      choices: ['MIT License', 'The Unilicense', 'Apache License 2.0', 'Mozilla Public License 2.0', 'Creative Commons Zero v1.0 Universal', 'GNU General Public Licesne', 'Boost Software License 1.0', 'Eclipse Public License v2.0'],
+    },
+    {
+        type: 'list',
+        name: 'installation',
+        message: 'What command should be run to install dependencies? Use default if unsure.',
+        choices: ['npm i'],
+      },
+      {
+        type: 'list',
+        name: 'tests',
+        message: 'What command should be run to run tests? Use default if unsure.',
+        choices: ['npm test'],
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'What does the user need to know about using the repo?',
+      },
+      {
+        type: 'input',
+        name: 'contributions',
+        message: 'What does the user need to know about contributing to the repo?',
+      },
+  ]);
+
+const generateREADME = (answers) =>
+  `![GitHub License](https://img.shields.io/github/license/${answers.username}/${answers.title})
+
+  # ${answers.title}
+
+  ${answers.description}
+
+  # Table of Contents
+  [License](https://github.com/${answers.username}/readme#license)
+
+  [Installation](https://github.com/${answers.username}/readme#installation)
+
+  [Usage](https://github.com/${answers.username}/readme#usage)
+
+  [Contributions](https://github.com/${answers.username}/readme#contributions)
+
+  [Tests](https://github.com/${answers.username}/readme#tests)
+
+  [Questions](https://github.com/${answers.username}/readme#questions)
+
+  
+
+  # License
+
+  ${answers.license}
+
+  # Installation
+
+  To install the dependencies needed in your coding software in order to use this generator, simply type ${answers.installation} into your terminal and press enter.
+
+  # Tests
+
+  To test if the packages are working, type ${answers.tests} into your terminal and press enter.
+
+  # Usage
+
+  ${answers.usage}
+
+  # Contributions
+
+  ${answers.contributions}
+
+
+  # Questions
+
+  If you have any questions about this project, feel free to reach me via github:
+  
+  ${answers.username} (https://github.com/${answers.username}/)
+
+  Or you can send me a message via email:
+  ${answers.email}
+  
+  `;
+
+promptUser()
+  .then((answers) => writeFileAsync('README.md', generateREADME(answers)))
+  .then(() => console.log('Successfully wrote README.md'))
+  .catch((err) => console.error(err));
+
+
